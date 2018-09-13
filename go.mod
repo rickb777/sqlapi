9fa275fd7c43135bcd18e69f4fb9cc565706526c
Rick <1358735+rickb777@users.noreply.github.com>
2018-09-13 23:03:37 +0100

    tests updated

--- go.mod
+++ go.mod
@@ -1,7 +1,10 @@
 module github.com/rickb777/sqlapi
 
 require (
+	github.com/benmoss/matchers v0.0.0-20170626211611-7907be6914c4
 	github.com/go-sql-driver/mysql v1.4.0
+	github.com/juju/loggo v0.0.0-20180524022052-584905176618 // indirect
+	github.com/juju/testing v0.0.0-20180820040200-b0b89ba330f2 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.0.0
 	github.com/mattn/go-sqlite3 v1.9.0
@@ -9,5 +12,6 @@ require (
 	golang.org/x/net v0.0.0-20180911220305-26e67e76b6c3 // indirect
 	google.golang.org/appengine v1.1.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
 	gopkg.in/yaml.v2 v2.2.1
 )
--- where/where_test.go
+++ where/where_test.go
@@ -1,29 +1,32 @@
 package where_test
 
 import (
-	"github.com/rickb777/sqlapi/schema"
-	. "github.com/rickb777/sqlapi/where"
-	"reflect"
 	"testing"
+	"github.com/benmoss/matchers"
+	"github.com/rickb777/sqlapi/schema"
+	"github.com/rickb777/sqlapi/where"
+	. "github.com/onsi/gomega"
 )
 
 func TestBuildWhereClause_happyCases(t *testing.T) {
-	nameEqFred := Eq("name", "Fred")
-	nameEqJohn := Eq("name", "John")
-	ageLt10 := Lt("age", 10)
-	ageGt5 := Gt("age", 5)
+	RegisterTestingT(t)
+
+	nameEqFred := where.Eq("name", "Fred")
+	nameEqJohn := where.Eq("name", "John")
+	ageLt10 := where.Lt("age", 10)
+	ageGt5 := where.Gt("age", 5)
 
 	cases := []struct {
-		wh          Expression
+		wh          where.Expression
 		expMysql    string
 		expPostgres string
 		expString   string
 		args        []interface{}
 	}{
-		{NoOp(), "", "", "", nil},
+		{where.NoOp(), "", "", "", nil},
 
 		{
-			Condition{"name", " not nil", nil},
+			where.Condition{"name", " not nil", nil},
 			"WHERE `name` not nil",
 			`WHERE "name" not nil`,
 			`WHERE name not nil`,
@@ -31,7 +34,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Condition{"p.name", " not nil", nil},
+			where.Condition{"p.name", " not nil", nil},
 			"WHERE `p`.`name` not nil",
 			`WHERE "p"."name" not nil`,
 			`WHERE p.name not nil`,
@@ -39,7 +42,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Null("name"),
+			where.Null("name"),
 			"WHERE `name` IS NULL",
 			`WHERE "name" IS NULL`,
 			`WHERE name IS NULL`,
@@ -47,7 +50,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			NotNull("name"),
+			where.NotNull("name"),
 			"WHERE `name` IS NOT NULL",
 			`WHERE "name" IS NOT NULL`,
 			`WHERE name IS NOT NULL`,
@@ -55,7 +58,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Condition{"name", " <>?", []interface{}{"Boo"}},
+			where.Condition{"name", " <>?", []interface{}{"Boo"}},
 			"WHERE `name` <>?",
 			`WHERE "name" <>$1`,
 			`WHERE name <>'Boo'`,
@@ -71,7 +74,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Like("name", "F%"),
+			where.Like("name", "F%"),
 			"WHERE `name` LIKE ?",
 			`WHERE "name" LIKE $1`,
 			`WHERE name LIKE 'F%'`,
@@ -79,7 +82,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			NoOp().And(nameEqFred),
+			where.NoOp().And(nameEqFred),
 			"WHERE (`name`=?)",
 			`WHERE ("name"=$1)`,
 			`WHERE (name='Fred')`,
@@ -87,7 +90,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			nameEqFred.And(Gt("age", 10)),
+			nameEqFred.And(where.Gt("age", 10)),
 			"WHERE (`name`=?) AND (`age`>?)",
 			`WHERE ("name"=$1) AND ("age">$2)`,
 			`WHERE (name='Fred') AND (age>10)`,
@@ -95,7 +98,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			nameEqFred.Or(Gt("age", 10)),
+			nameEqFred.Or(where.Gt("age", 10)),
 			"WHERE (`name`=?) OR (`age`>?)",
 			`WHERE ("name"=$1) OR ("age">$2)`,
 			`WHERE (name='Fred') OR (age>10)`,
@@ -103,7 +106,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			nameEqFred.And(ageGt5).And(Gt("weight", 15)),
+			nameEqFred.And(ageGt5).And(where.Gt("weight", 15)),
 			"WHERE (`name`=?) AND (`age`>?) AND (`weight`>?)",
 			`WHERE ("name"=$1) AND ("age">$2) AND ("weight">$3)`,
 			`WHERE (name='Fred') AND (age>5) AND (weight>15)`,
@@ -111,7 +114,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			nameEqFred.Or(ageGt5).Or(Gt("weight", 15)),
+			nameEqFred.Or(ageGt5).Or(where.Gt("weight", 15)),
 			"WHERE (`name`=?) OR (`age`>?) OR (`weight`>?)",
 			`WHERE ("name"=$1) OR ("age">$2) OR ("weight">$3)`,
 			`WHERE (name='Fred') OR (age>5) OR (weight>15)`,
@@ -119,7 +122,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Between("age", 12, 18).Or(Gt("weight", 45)),
+			where.Between("age", 12, 18).Or(where.Gt("weight", 45)),
 			"WHERE (`age` BETWEEN ? AND ?) OR (`weight`>?)",
 			`WHERE ("age" BETWEEN $1 AND $2) OR ("weight">$3)`,
 			`WHERE (age BETWEEN 12 AND 18) OR (weight>45)`,
@@ -127,7 +130,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			GtEq("age", 10),
+			where.GtEq("age", 10),
 			"WHERE `age`>=?",
 			`WHERE "age">=$1`,
 			`WHERE age>=10`,
@@ -135,7 +138,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			LtEq("age", 10),
+			where.LtEq("age", 10),
 			"WHERE `age`<=?",
 			`WHERE "age"<=$1`,
 			`WHERE age<=10`,
@@ -143,7 +146,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			NotEq("age", 10),
+			where.NotEq("age", 10),
 			"WHERE `age`<>?",
 			`WHERE "age"<>$1`,
 			`WHERE age<>10`,
@@ -151,7 +154,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			In("age", 10, 12, 14),
+			where.In("age", 10, 12, 14),
 			"WHERE `age` IN (?,?,?)",
 			`WHERE "age" IN ($1,$2,$3)`,
 			"WHERE age IN (10,12,14)",
@@ -159,7 +162,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			In("age", []int{10, 12, 14}),
+			where.In("age", []int{10, 12, 14}),
 			"WHERE `age` IN (?,?,?)",
 			`WHERE "age" IN ($1,$2,$3)`,
 			"WHERE age IN (10,12,14)",
@@ -167,7 +170,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Not(nameEqFred),
+			where.Not(nameEqFred),
 			"WHERE NOT (`name`=?)",
 			`WHERE NOT ("name"=$1)`,
 			`WHERE NOT (name='Fred')`,
@@ -175,7 +178,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Not(nameEqFred.And(ageLt10)),
+			where.Not(nameEqFred.And(ageLt10)),
 			"WHERE NOT ((`name`=?) AND (`age`<?))",
 			`WHERE NOT (("name"=$1) AND ("age"<$2))`,
 			`WHERE NOT ((name='Fred') AND (age<10))`,
@@ -183,7 +186,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Not(nameEqFred.Or(ageLt10)),
+			where.Not(nameEqFred.Or(ageLt10)),
 			"WHERE NOT ((`name`=?) OR (`age`<?))",
 			`WHERE NOT (("name"=$1) OR ("age"<$2))`,
 			`WHERE NOT ((name='Fred') OR (age<10))`,
@@ -191,7 +194,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Not(nameEqFred).And(ageLt10),
+			where.Not(nameEqFred).And(ageLt10),
 			"WHERE (NOT (`name`=?)) AND (`age`<?)",
 			`WHERE (NOT ("name"=$1)) AND ("age"<$2)`,
 			`WHERE (NOT (name='Fred')) AND (age<10)`,
@@ -199,7 +202,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Not(nameEqFred).Or(ageLt10),
+			where.Not(nameEqFred).Or(ageLt10),
 			"WHERE (NOT (`name`=?)) OR (`age`<?)",
 			`WHERE (NOT ("name"=$1)) OR ("age"<$2)`,
 			`WHERE (NOT (name='Fred')) OR (age<10)`,
@@ -207,7 +210,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			And(nameEqFred, ageLt10),
+			where.And(nameEqFred, ageLt10),
 			"WHERE (`name`=?) AND (`age`<?)",
 			`WHERE ("name"=$1) AND ("age"<$2)`,
 			`WHERE (name='Fred') AND (age<10)`,
@@ -215,7 +218,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			And(nameEqFred).And(And(ageLt10)),
+			where.And(nameEqFred).And(where.And(ageLt10)),
 			"WHERE (`name`=?) AND (`age`<?)",
 			`WHERE ("name"=$1) AND ("age"<$2)`,
 			`WHERE (name='Fred') AND (age<10)`,
@@ -223,7 +226,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Or(nameEqFred, ageLt10),
+			where.Or(nameEqFred, ageLt10),
 			"WHERE (`name`=?) OR (`age`<?)",
 			`WHERE ("name"=$1) OR ("age"<$2)`,
 			`WHERE (name='Fred') OR (age<10)`,
@@ -231,7 +234,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			And(nameEqFred.Or(nameEqJohn), ageLt10),
+			where.And(nameEqFred.Or(nameEqJohn), ageLt10),
 			"WHERE ((`name`=?) OR (`name`=?)) AND (`age`<?)",
 			`WHERE (("name"=$1) OR ("name"=$2)) AND ("age"<$3)`,
 			`WHERE ((name='Fred') OR (name='John')) AND (age<10)`,
@@ -239,7 +242,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Or(nameEqFred, ageLt10.And(ageGt5)),
+			where.Or(nameEqFred, ageLt10.And(ageGt5)),
 			"WHERE (`name`=?) OR ((`age`<?) AND (`age`>?))",
 			`WHERE ("name"=$1) OR (("age"<$2) AND ("age">$3))`,
 			`WHERE (name='Fred') OR ((age<10) AND (age>5))`,
@@ -247,7 +250,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Or(nameEqFred, nameEqJohn).And(ageGt5),
+			where.Or(nameEqFred, nameEqJohn).And(ageGt5),
 			"WHERE ((`name`=?) OR (`name`=?)) AND (`age`>?)",
 			`WHERE (("name"=$1) OR ("name"=$2)) AND ("age">$3)`,
 			`WHERE ((name='Fred') OR (name='John')) AND (age>5)`,
@@ -255,7 +258,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Or(nameEqFred, nameEqJohn, And(ageGt5)),
+			where.Or(nameEqFred, nameEqJohn, where.And(ageGt5)),
 			"WHERE (`name`=?) OR (`name`=?) OR ((`age`>?))",
 			`WHERE ("name"=$1) OR ("name"=$2) OR (("age">$3))`,
 			`WHERE (name='Fred') OR (name='John') OR ((age>5))`,
@@ -263,7 +266,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			Or().Or(NoOp()).And(NoOp()),
+			where.Or().Or(where.NoOp()).And(where.NoOp()),
 			"",
 			"",
 			"",
@@ -271,7 +274,7 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 
 		{
-			And(Or(NoOp())),
+			where.And(where.Or(where.NoOp())),
 			"",
 			"",
 			"",
@@ -279,79 +282,63 @@ func TestBuildWhereClause_happyCases(t *testing.T) {
 		},
 	}
 
-	for i, c := range cases {
+	for _, c := range cases {
 		sql, args := c.wh.Build(schema.Mysql)
 
-		if sql != c.expMysql {
-			t.Errorf("%d Mysql: Wanted %s\nGot %s", i, c.expMysql, sql)
-		}
-
-		if !reflect.DeepEqual(args, c.args) {
-			t.Errorf("%d Mysql: Wanted %v\nGot %v", i, c.args, args)
-		}
+		Ω(sql).Should(Equal(c.expMysql))
+		Ω(args).Should(matchers.DeepEqual(c.args))
 
 		sql, args = c.wh.Build(schema.Postgres)
 
-		if sql != c.expPostgres {
-			t.Errorf("%d Postgres: Wanted %s\nGot %s", i, c.expPostgres, sql)
-		}
-
-		if !reflect.DeepEqual(args, c.args) {
-			t.Errorf("%d Postgres: Wanted %v\nGot %v", i, c.args, args)
-		}
+		Ω(sql).Should(Equal(c.expPostgres))
+		Ω(args).Should(matchers.DeepEqual(c.args))
 
 		s := c.wh.String()
 
-		if s != c.expString {
-			t.Errorf("%d String: Wanted %s\nGot %s", i, c.expString, s)
-		}
+		Ω(s).Should(Equal(c.expString))
 	}
 }
 
 func TestQueryConstraint(t *testing.T) {
+	RegisterTestingT(t)
+
 	cases := []struct {
-		qc          QueryConstraint
+		qc          where.QueryConstraint
 		expSqlite   string
 		expMysql    string
 		expPostgres string
 	}{
 		{nil, "", "", ""},
-		{Literal("order by foo"), "order by foo", "order by foo", "order by foo"},
-		{OrderBy("foo").Asc(), "ORDER BY `foo`", "ORDER BY `foo`", `ORDER BY "foo"`},
-		{OrderBy("foo").Desc(), "ORDER BY `foo` DESC", "ORDER BY `foo` DESC", `ORDER BY "foo" DESC`},
-		{OrderBy("foo").OrderBy("bar"), "ORDER BY `bar`", "ORDER BY `bar`", `ORDER BY "bar"`},
-		{Limit(0), "", "", ""},
-		{Limit(10), "LIMIT 10", "LIMIT 10", "LIMIT 10"},
-		{Offset(20), "OFFSET 20", "OFFSET 20", "OFFSET 20"},
-		{Limit(5).OrderBy("foo", "bar"), "ORDER BY `foo`,`bar` LIMIT 5", "ORDER BY `foo`,`bar` LIMIT 5", `ORDER BY "foo","bar" LIMIT 5`},
-		{OrderBy("foo").Desc().Limit(10).Offset(20), "ORDER BY `foo` DESC LIMIT 10 OFFSET 20", "ORDER BY `foo` DESC LIMIT 10 OFFSET 20", `ORDER BY "foo" DESC LIMIT 10 OFFSET 20`},
+		{where.Literal("order by foo"), "order by foo", "order by foo", "order by foo"},
+		{where.OrderBy("foo").Asc(), "ORDER BY `foo`", "ORDER BY `foo`", `ORDER BY "foo"`},
+		{where.OrderBy("foo").Desc(), "ORDER BY `foo` DESC", "ORDER BY `foo` DESC", `ORDER BY "foo" DESC`},
+		{where.OrderBy("foo").OrderBy("bar"), "ORDER BY `bar`", "ORDER BY `bar`", `ORDER BY "bar"`},
+		{where.Limit(0), "", "", ""},
+		{where.Limit(10), "LIMIT 10", "LIMIT 10", "LIMIT 10"},
+		{where.Offset(20), "OFFSET 20", "OFFSET 20", "OFFSET 20"},
+		{where.Limit(5).OrderBy("foo", "bar"), "ORDER BY `foo`,`bar` LIMIT 5", "ORDER BY `foo`,`bar` LIMIT 5", `ORDER BY "foo","bar" LIMIT 5`},
+		{where.OrderBy("foo").Desc().Limit(10).Offset(20), "ORDER BY `foo` DESC LIMIT 10 OFFSET 20", "ORDER BY `foo` DESC LIMIT 10 OFFSET 20", `ORDER BY "foo" DESC LIMIT 10 OFFSET 20`},
 	}
 
-	for i, c := range cases {
+	for _, c := range cases {
 		var sql string
 
 		if c.qc != nil {
-			sql = BuildQueryConstraint(c.qc, schema.Sqlite)
+			sql = where.BuildQueryConstraint(c.qc, schema.Sqlite)
 		}
 
-		if sql != c.expSqlite {
-			t.Errorf("%d Sqlite: Wanted %s\nGot %s", i, c.expSqlite, sql)
-		}
+		Ω(sql).Should(Equal(c.expSqlite))
 
 		if c.qc != nil {
-			sql = BuildQueryConstraint(c.qc, schema.Mysql)
+			sql = where.BuildQueryConstraint(c.qc, schema.Mysql)
 		}
 
-		if sql != c.expMysql {
-			t.Errorf("%d Mysql: Wanted %s\nGot %s", i, c.expMysql, sql)
-		}
+		Ω(sql).Should(Equal(c.expMysql))
 
 		if c.qc != nil {
-			sql = BuildQueryConstraint(c.qc, schema.Postgres)
+			sql = where.BuildQueryConstraint(c.qc, schema.Postgres)
 		}
 
-		if sql != c.expPostgres {
-			t.Errorf("%d Postgres: Wanted %s\nGot %s", i, c.expPostgres, sql)
-		}
+		Ω(sql).Should(Equal(c.expPostgres))
 	}
 }
