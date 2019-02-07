b861d3ab0b89f8ab54a588e27b7999d7b6a03d32
Rick <1358735+rickb777@users.noreply.github.com>
2019-02-07 16:37:59 +0000

    fixed nil tags in field

--- version.sh
+++ version.sh
@@ -3,6 +3,7 @@
 VFILE=util/version.go
 
 echo "// Updated automatically" > $VFILE
+echo "" >> $VFILE
 echo "package util" >> $VFILE
 echo "" >> $VFILE
-echo "const Version    = \"$(git describe --tags 2>/dev/null)\"" >> $VFILE
+echo "const Version = \"$(git describe --tags 2>/dev/null)\"" >> $VFILE
